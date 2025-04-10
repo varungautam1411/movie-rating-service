ec15135cdfcd30302a64688915d274ca012f31a0
varungautam1411 <archervg@gmail.com>
2025-04-10 12:04:43 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+APP_HOME="/home/ec2-user/restApp"
+JAR_FILE="$APP_HOME/movie-rating-service-1.0.jar"
+CONFIG_DIR="$APP_HOME/config"
+
+java -jar $JAR_FILE \
+    --spring.config.location=file:$CONFIG_DIR/application.properties
