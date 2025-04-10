c00d2d59ec698642db850511c56e1983fbbffea9
varungautam1411 <archervg@gmail.com>
2025-04-10 12:20:00 -0700

    Update MovieRating.java

--- src/main/java/com/example/model/MovieRating.java
+++ src/main/java/com/example/model/MovieRating.java
@@ -16,18 +16,5 @@ public class MovieRating {
     private String date;
 }
 
-// Movie.java
-package com.example.model;
-
-import lombok.Data;
-import java.util.List;
-
-@Data
-public class Movie {
-    private String movieId;
-    private String title;
-    private int yearOfRelease;
-    private List<MovieRating> watchedBy;
-}
 
     
