5af862f9b03460b5e92c80964133ed5e338aa7b2
varungautam1411 <archervg@gmail.com>
2025-04-10 11:57:00 -0700

    Update MovieRating.java

--- src/main/java/com/example/model/MovieRating.java
+++ src/main/java/com/example/model/MovieRating.java
@@ -1,1 +1,33 @@
+// MovieRating.java
+package com.example.model;
 
+import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Data
+public class MovieRating {
+    @JsonProperty("customer-id")
+    private String customerId;
+    
+    @JsonProperty("movie-id")
+    private String movieId;
+    
+    private int rating;
+    private String date;
+}
+
+// Movie.java
+package com.example.model;
+
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class Movie {
+    private String movieId;
+    private String title;
+    private int yearOfRelease;
+    private List<MovieRating> watchedBy;
+}
+
+    
